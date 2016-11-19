af29ba0799013e3e36e38f4a9131f30260c12c4e
Nastin <nastin.malina@gmail.com>
2016-11-19 16:44:31 +0200

    создала массив чисел

--- js/sudoku.js
+++ js/sudoku.js
@@ -3,11 +3,6 @@ inputWrapper.className = 'inputWrapper';
 var resultWrapper = document.createElement('div');
 resultWrapper.className = 'resultWrapper';
 
-
-var hendler =function (event) {
-  console.log(event.target);
-}
-
 for (var k = 0; k < 9; k++) {
   var inputContainer = document.createElement('div');
   inputContainer.className = 'inputContainer';
@@ -38,3 +33,24 @@ button.textContent = "Решить";
 document.body.appendChild(inputWrapper);
 document.body.appendChild(resultWrapper);
 document.body.appendChild(button);
+
+var numbers = [];
+// создала массив массивов
+for (var i = 0; i < 9; i++) {
+  var block = [];
+  for (var i = 0; i < 9; i++) {
+    block.push(null);
+  }
+  numbers.push(block);
+}
+
+var inputs = inputWrapper.getElementsByTagName('input');
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('keyup', function(e) {
+    // нажали  цифру или удаление
+    if ((e.keyCode >= 46 && e.keyCode <= 57) || e.keyCode == 8) {
+      console.log(e.target.value);
+    }
+  });
+}
